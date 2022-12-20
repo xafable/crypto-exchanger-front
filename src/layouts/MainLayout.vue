450387edb4a4e82e80a123a9724882a9d8c0d825
xafable <xafable55@gmail.com>
2022-12-20 22:13:14 +0200

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+
+    <q-header class="bg-dark text-white" height-hint="98">
+      <div class="c-container">
+        <div class="row justify-between">
+          <div class="col-4">
+            <q-img
+              :src="'https://i.ibb.co/mR49B6K/Pngtree-simple-logo-vector-3174709.png'"
+              spinner-color="white"
+              style="height: 40px;width: 480px;margin-bottom: 30px"
+            />
+          </div>
+          <div class="col-6" style="padding-left: 15%">
+            <q-btn  stretch flat label="Home" />
+            <q-btn  stretch flat label="Our coins" />
+            <q-btn  stretch flat label="Reviews" />
+            <q-btn  stretch flat label="Contact" />
+
+          </div>
+          <div class="col-2">
+
+            <q-btn  rounded color="secondary" label="Change crypto" />
+
+          </div>
+        </div>
+
+
+
+      </div>
+
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+  components: {
+
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+<style scoped>
+.c-q-tabs {
+  padding-right: 20%;
+}
+.c-container {
+  margin-top: 15px;
+  width: 80%;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,188 @@
+<template>
+  <q-page class="">
+
+
+      <div style="background-color: #1D1D1D;position: relative">
+        <div class="c-container">
+          <div class="row justify-between" style="padding-top: 50px">
+            <transition name="slide" appear>
+            <div class="col-4">
+              <div class="row" style="padding-top: 30px">
+                <p class="text-h1 text-white">Make crypto </p>
+              </div>
+              <div class="row">
+                <p class="text-h1 text-brand">exchanged easily </p>
+              </div>
+              <div class="row">
+                <p class="text-white">Fast processing. Low commissions. Best conditions. </p>
+              </div>
+              <q-btn rounded color="secondary" label="Change crypto" />
+            </div>
+            </transition>
+            <div class="col-4">
+              <img src="https://i.ibb.co/ZWDfRTJ/yyyyy7.png" style="height:100%; "/>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+
+    <div style="background-color: #ffffff;">
+      <div class="c-container benefits__wrap"  >
+        <div class="q-pa-md row q-gutter-md justify-center">
+
+          <q-card dark bordered class="bg-secondary my-card">
+            <q-card-section>
+              <div class="text-h6">
+                <q-avatar
+                  key="md"
+                  size="md"
+                  color="primary"
+                  text-color="white"
+                  icon="directions"
+                />
+                Best exchange rate of crypto</div>
+
+            </q-card-section>
+            <q-separator dark inset />
+            <q-card-section>
+              Automated system for processing applications. Tools for controlling status and time of the converting.
+            </q-card-section>
+          </q-card>
+
+          <q-card dark bordered class="bg-secondary my-card">
+            <q-card-section>
+              <div class="text-h6">Dynamic crypto market rates</div>
+              <div class="text-subtitle2"></div>
+            </q-card-section>
+            <q-separator dark inset />
+            <q-card-section>
+              Always the most profitable offer on the market. WiseSwap makes a rate fixation while you filling in the application.
+            </q-card-section>
+          </q-card>
+
+          <q-card dark bordered class="bg-secondary my-card">
+            <q-card-section>
+              <div class="text-h6">Protected exchange report</div>
+              <div class="text-subtitle2">by John Doe</div>
+            </q-card-section>
+            <q-separator dark inset />
+            <q-card-section>
+              We provide an easy protected application report for our clients. Detailed information in real time.
+            </q-card-section>
+          </q-card>
+
+          <q-card dark bordered class="bg-secondary my-card">
+            <q-card-section>
+              <div class="text-h6">U-Swap Support 24/7</div>
+              <div class="text-subtitle2">by John Doe</div>
+            </q-card-section>
+            <q-separator dark inset />
+            <q-card-section>
+              Our support service is always ready to help you. Telegram, Email or Contact form is on our website.
+            </q-card-section>
+          </q-card>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div style="background-color: #ffffff;position: relative;margin-top: 200px">
+      <div class="c-container">
+        <div class="row justify-between" style="padding-top: 50px">
+            <div class="col-8">
+              <div class="row">
+                <q-select rounded outlined v-model="model" :options="options" label="Rounded outlined" />
+                <q-input rounded outlined v-model="text" label="Rounded outlined" />
+              </div>
+
+            </div>
+
+          <div class="col-4">
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+<style scoped>
+.c-container {
+  width: 80%;
+  margin-right: auto;
+  margin-left: auto;
+}
+.text-brand {
+  color: #39C9C6 !important;
+}
+.text-white {
+  color: #ffffff !important;
+}
+.benefits__wrap {
+  gap: 24px;
+  position: relative;
+  z-index: 1;
+  margin-top: -94px;
+}
+
+</style>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+
+</style>
+
+<style lang="scss" scoped>
+.slide-enter {
+  opacity: 0;
+}
+
+.slide-enter-active {
+  animation: slide-in 1s ease-in forwards;
+  transition: opacity .5s;
+}
+
+.slide-leave {
+  // initial state set in keyframe
+}
+
+.slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  opacity: 0;
+  transition: opacity 1s;
+}
+
+
+@keyframes slide-in {
+  from {
+    transform: translateX(-1200px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(120px);
+  }
+}
+</style>
