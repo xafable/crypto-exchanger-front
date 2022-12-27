46cfb4cd43f7773c8f584194f5070e8152fcbb78
xafable <xafable55@gmail.com>
2022-12-27 13:58:45 +0200

    config edited

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/documents', component: () => import('pages/DocumentsPage.vue')},
+      { path: '/application/:id', component: () => import('pages/PaymentPage.vue')}
+
     ]
   },
 
