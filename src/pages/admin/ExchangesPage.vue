dacb367dac1e466030fb052f8cbcd928e6b1cbe1
xafable <xafable55@gmail.com>
2023-01-21 11:26:19 +0200

    Update ExchangesPage.vue

--- src/pages/admin/ExchangesPage.vue
+++ src/pages/admin/ExchangesPage.vue
@@ -3,23 +3,26 @@
     <q-markup-table>
       <thead>
       <tr>
-        <th class="text-left">Dessert (100g serving)</th>
-        <th class="text-right">Calories</th>
-        <th class="text-right">Fat (g)</th>
-        <th class="text-right">Carbs (g)</th>
+        <th class="text-left">ApplicationId</th>
+        <th class="text-right">Coin</th>
+        <th class="text-right">Amount</th>
+        <th class="text-right">Status</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="item in rows" :key="item.calories">
-        <td class="text-left">{{ item.name }}</td>
-        <td class="text-right">{{ item.calories }}</td>
-        <td class="text-right">{{ item.fat }}</td>
+      <tr v-for="item in applications" :key="item.calories">
+        <td class="text-left">{{ item.ApplicationId }}</td>
+        <td class="text-right">{{ item.fromCoin.title }} -> {{ item.toCoin.title }}</td>
         <td class="text-right">
+          <q-badge color="primary">{{ item.fromCoin.amount }}</q-badge> -> {{ item.toCoin.amount }}
+        </td>
+        <td>
           <q-select
-            @update:model-value="tes($event,22)"
-            v-model="model"
+            color="secondary"
+            @update:model-value="tes($event,item.ApplicationId)"
+            v-model="item.status.description"
             :options="options"
-            label="Standard" />
+            label="Status" />
         </td>
 
       </tr>
@@ -55,19 +58,47 @@ export default {
   },
   data() {
     return {
-      model: '',
-      options: [
-        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-      ]
+      model:[],
+      options: [],
+      applications: [],
+      dataLoaded: false
     }
   },
   methods:{
     tes(it,r){
-      //alert(it)
       console.log(r)
     },
-    async loadCoins(){
-      await fetch('https://exapi.vikarecept.com/api/v1/coins/available',{
+    async loadStatuses(){
+      await fetch('https://exapi.vikarecept.com/api/v1/statuses',{
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+        },
+      })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          else
+            return response.json().then(data => { throw new Error(data.message) })
+        })
+        .then(data => {
+          let opt = []
+          data.data.forEach(function (item) {
+            opt.push({
+              label: item.description,
+              value: item.title
+            })
+          })
+          this.options = opt
+          this.dataLoaded = true
+
+        })
+        .catch( error =>  {
+        })
+    },
+    async loadApplications(){
+      await fetch('https://exapi.vikarecept.com/api/v1/application/load',{
         method: 'GET',
         headers: {
           'Accept': 'application/json',
@@ -82,22 +113,17 @@ export default {
         })
         .then(data => {
 
-          let opt = []
-          data.data.forEach(function (item) {
-            opt.push({
-              label: item.title,
-              value: item
-            })
-          })
-          this.options = opt
+          this.applications = data.data
           this.dataLoaded = true
 
         })
         .catch( error =>  {
-          this.errorNotify()
         })
     },
-
+  },
+  created() {
+    this.loadStatuses()
+    this.loadApplications()
   }
 }
 </script>
